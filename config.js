66ded1f04c4965c59df49b814d51840da843422e
Rizki Pratama <rprtma187@gmail.com>
2024-12-18 04:23:09 +0700

    Ultah dedel

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Selamat Ulang Tahun",
+    introTitle: 'Dedel Ku',
+    introDesc: `Mau Lihat Sesuatu gakk??`,
+    btnIntro: 'Mau dong',
+    title: '1, 2, 3 Jeng Jeng. Selamat Ulang Tahun Yak.',
+    desc: 'Mau Bareng Terus Sama Aku Gak',
+    btnYes: 'Muall laa',
+    btnNo: 'Ndakk Ahhh',
+    question: 'Semoga selalu sehat, panjang umur, makin cantik, makin pintar, dan pastinya makin sayang aku! Aku tu Cayang banget sama kamu, dan aku yakin kamu bakal terus sukses ke depannya, Semangatt Untuk sempHas besok, yee! dedel pasti bisa dong, jangan takut, semua pasti baik-baik aja. Aku selalu di sini buat you, siap kasih dukungan dan semangat! Semoga tahun ini dan seterusnya penuh dengan kebahagiaan dan semua impian kamu bisa tercapai. Aku sayang banget sama kamu dan selalu support apapun yang kamu lakukan. Let’s make this year amazing together',
+    btnReply: 'Sama - Sama',
+    reply: 'Delia, kamu adalah teman yang luar biasa dan aku bersyukur bisa merayakan hari ini bersama kamu! <33333333',
+    mess: 'Love You Beb. Semangat SemHAs besok yaps!',
+    messDesc: 'Awas Terbirak Yang, HEHEHE',
+    btnAccept: 'Oke, siap Boskuu!',
+    messLink: 'https://api.whatsapp.com/send/?phone=628217848126' //link WhatsApp
+}
